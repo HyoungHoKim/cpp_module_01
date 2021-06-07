bb48809533d1f61b17a2f26afb010ee3326baf8e
hyoukim <dkdemfp2@naver.com>
2021-06-07 16:33:10 +0900

    random fix

--- ex02/ZombieEvent.cpp
+++ ex02/ZombieEvent.cpp
@@ -12,21 +12,22 @@ Zombie	*ZombieEvent::newZombie(std::string name)
 
 void	ZombieEvent::randomChump(void)
 {
-	std::string name_pool[7] = {
+	std::string name_pool[8] = {
 		"red",
 		"blue",
 		"green",
 		"yellow",
 		"pink", 
 		"black",
-		"white"
+		"white",
+		"rainbow"
 	};
 	int num;
 	std::string random_name;
 	Zombie *new_zombie;
 
 	srand((unsigned int)time(NULL));
-	num = rand() % 7;
+	num = rand() % 8;
 	random_name = name_pool[num];
 	new_zombie = new Zombie(this->type, random_name);
 	new_zombie->announce();
